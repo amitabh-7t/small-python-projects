58dc8513ea603c117c8e64fcde903fc3c76f29d6
AMITABH THAKUR <ENG23AM0215@DSU.EDU.IN>
2024-10-27 09:57:03 +0530

    Expense Splitter 💸
    A simple Python program to help you easily split expenses among multiple people based on individual contribution percentages. Whether for group outings, shared bills, or any collective spending, Expense Splitter allows you to calculate each person's share accurately and avoid confusion!
    Features
    
        Custom Splitting by Percentage: Allocate different split percentages for each person, enabling fair distribution.
        Error Handling: Avoids common input errors, with validation for both numerical inputs and logical distribution checks.
        Currency Option: Displays the amount owed in INR, customizable for different currencies as needed.
    
    How it Works
    
        Enter the number of participants.
        Input the total expense amount.
        Specify each person’s share percentage.
        Receive a breakdown showing each person's contribution. The program also verifies if the split percentages sum up to the total expense to ensure accuracy.
    
    Usage Example
    
    bash
    
    $ python expense_splitter.py
    Enter the number of persons: 3
    Enter the total expense: 1500
    Enter the split percentage for:
    person 1: 50
    person 2: 30
    person 3: 20

---
+++ expense split/expensesplit.py
@@ -0,0 +1,43 @@
+def expense_splitter(no_of_people, price):
+    total_expense = price
+    splitted_expense = total_expense / no_of_people
+    return splitted_expense
+
+
+        
+
+
+def main():
+    while True:  # Loop until valid inputs are given
+        try:
+            no_of_people = int(input("Enter the number of persons: "))
+            price = float(input("Enter the total expense: "))
+            currency = "INR"
+            distribution ={}
+            sum=0
+            print("enter the split percentage for ")
+            for distri in range(no_of_people) :
+                key = distri
+                value = float(input(f"person {distri+1} :"))
+                value = price * (value/100)
+                distribution[key] = value
+                sum = sum+value
+
+            print("----- - -- --- -- - -----")
+            if sum == price :
+                for key, value in distribution.items():
+                    print(f"person {key+1} will pay {currency} {value} ")  # Prints each key-value pair
+            else :
+                    print("distribution is not corect")
+
+            print("----- - -- --- -- - -----")
+
+            # print(f"Per person expense: {currency} {expense_splitter(no_of_people, price)}")
+            break  # Break the loop if no exception occurs
+        except ZeroDivisionError:
+            print("Error: Number of people cannot be zero. Please enter a valid number.")
+        except ValueError:
+            print("Error: Invalid input. Please enter numbers only.")
+        
+# Start the program
+main()
